5d28fed0c97868a016438ca84336009398520a75
Alan Klinger <klingerkrieg@gmail.com>
2017-11-22 10:20:10 -0300

    Refatoramento

--- ConfigEditor/js/main.js
+++ ConfigEditor/js/main.js
@@ -4,6 +4,30 @@ var modelIncludes = [{url:"kinectOptions.html",onLoad:function(){}},
                     {url:"leapMotionOptions.html",onLoad:function(){}},
                     {url:"keyboardMouseOptions.html",onLoad:loadKeyboardOptions}];
 
+
+
+
+var actions = [{class:"handTop", title:"[Kinect] Altura da mão"},
+                    {class:"kinectWalking", title:"[Kinect] Marcha estacionária"},
+                    {class:"kinectStep", title:"[Kinect] Step"},
+                    {class:"kinectBalance", title:"[Kinect] Equilíbrio"},
+                    {class:"kinectFist", title:"[Kinect] Flexão de punho"},
+                    {class:"kinectTurn", title:"[Kinect] Girar corpo"},
+                    {class:"kinectBody", title:"[Kinect] Inclinar corpo"}
+                  ];
+
+var leapMotionActions = [{class:"leapCloseHand", title:"[LeapMotion] Fechar mão"},
+                  {class:"leapPinch", title:"[LeapMotion] Movimento de pinça"},
+                  {class:"leapFist", title:"[LeapMotion] Flexionar punho"}
+                ];
+
+var keyboardMouseActions = [{class:"keyboard", title:"Teclado"},
+                {class:"mouse", title:"Mouse"}
+              ]
+
+actions = actions.concat(leapMotionActions);
+actions = actions.concat(keyboardMouseActions);
+
 $(function(){
 
     for (var i = 0; i < modelIncludes.length; i++){
@@ -34,17 +58,17 @@ $(function(){
         $('#mapView').children().each(function(i,item){
             item = $(item);
             
-            for (var y = 0; y < kinectActions.length; y++){
-                if (item.hasClass(kinectActions[y].class)){
-                    jsonConfig.push(kinectActions[y].func(item));
+            for (var y = 0; y < actions.length; y++){
+                if (item.hasClass(actions[y].class)){
+                    jsonConfig.push(formToJSON(item));
                 }
             }
 
         });
         
-        console.log(jsonConfig);
+        
         jsonStr = JSON.stringify(jsonConfig);
-
+        console.log(jsonStr);
         fs.writeFile('config.json', jsonStr, 'utf8', function(msg){
             //console.log(msg);
         });
@@ -60,10 +84,6 @@ function removeMap(el){
 
 function addCommands(){
     cmds = $('#command');
-    
-    actions = kinectActions;
-    actions = actions.concat(leapMotionActions);
-    actions = actions.concat(keyboardMouseActions);
 
     for (var y = 0; y < actions.length; y++){
         cmds.append("<option value='"+actions[y].class+"'>"+actions[y].title+"</option>");
@@ -71,7 +91,7 @@ function addCommands(){
 }
 
 function addActions(){
-    act = $('#actionModel #action');
+    act = $('#actionModel #toKey');
 
     keys = getKeyboardKeys();
     keys.push({t:"Definir posição",code:"KINECT_DETERMINE_CENTER_POS"});
@@ -86,8 +106,7 @@ function addActions(){
 
 function showExtraActions(el){
     el = $(el);
-    console.log(el.val());
-
+    
     parent = $(el).parent();
     if (el.val() == "VK_MOUSEMOVE"){
         parent.find("#moveMouseSpace").show();
@@ -101,3 +120,82 @@ function showExtraActions(el){
         parent.find("#msgSpace").hide();
     }
 }
+
+
+function formToJSON(el){
+    json = {};
+    json.type = "key";
+    el.find(".toJSON").each(function(i,el){
+        el = $(el);
+        json[el.attr('id')] = el.val();
+
+        div = el.attr("divideby");
+        if (div != undefined){
+            json[el.attr('id')] = parseInt(json[el.attr('id')])/parseInt(div);
+        }
+    });
+
+
+    if ( json.toKey == "VK_MOUSEMOVE"){
+        json.mouseX = parent.find("#moveMouseSpace #x").val();
+        json.mouseY = parent.find("#moveMouseSpace #y").val();
+    } else 
+    if ( json.toKey == "ALERT" || el.val() == "MESSAGE"){
+        json.msg = parent.find("#msgSpace #msg").val();
+    }
+
+    return json;
+}
+
+
+
+function getKeyboardKeys(){
+    keys = [{t:"ESC",code:"VK_ESCAPE"},
+    {t:"F1",code:"VK_F1"},
+    {t:"F2",code:"VK_F2"},
+    {t:"F3",code:"VK_F3"},
+    {t:"F4",code:"VK_F4"},
+    {t:"F5",code:"VK_F5"},
+    {t:"F6",code:"VK_F6"},
+    {t:"F7",code:"VK_F7"},
+    {t:"F8",code:"VK_F8"},
+    {t:"F9",code:"VK_F9"},
+    {t:"F10",code:"VK_F10"},
+    {t:"F11",code:"VK_F11"},
+    {t:"F12",code:"VK_F12"},
+    {t:"TAB",code:"VK_TAB"},
+    {t:"CAPS LOCK",code:"VK_CAPITAL"},
+    {t:"SHIFT (Esq)",code:"VK_LSHIFT"},
+    {t:"SHIFT (Dir)",code:"VK_RSHIFT"},
+    {t:"ENTER",code:"VK_RETURN"},
+    {t:"CTRL",code:"VK_CONTROL"},
+    {t:"ALT",code:"VK_MENU"},        
+    {t:"ESPAÇO",code:"VK_SPACE"},        
+    {t:"HOME",code:"VK_HOME"},        
+    {t:"END",code:"VK_END"},        
+    {t:"INSERT",code:"VK_INSERT"},        
+    {t:"DELETE",code:"VK_DELETE"},
+    {t:"PrtScreen",code:"VK_SNAPSHOT"},        
+    {t:"Seta cima",code:"VK_UP"},        
+    {t:"Seta direita",code:"VK_RIGHT"},        
+    {t:"Seta baixo",code:"VK_DOWN"},        
+    {t:"Seta esquerda",code:"VK_LEFT"}
+  ];
+  
+    for (var i = 0; i <= 9; i++){
+        keys.push({t:i,code:i});
+    }
+    for(var i = 9; i < 36 ;i++){
+        chr = i.toString(36).toUpperCase();
+        keys.push({t:chr,code:chr});
+    }
+    return keys;
+  }
+  
+  function loadKeyboardOptions(){
+    act = $('#keyboardModel #from');
+    keys = getKeyboardKeys();
+    for(var i = 0; i < keys.length ;i++){
+        act.append("<option value='"+keys[i].code+"'>"+keys[i].t+"</option>");
+    }
+  }
